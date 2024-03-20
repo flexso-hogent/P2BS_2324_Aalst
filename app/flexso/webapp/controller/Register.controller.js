7d9d7674c8581e7cfc6831ffafa4d67776071981
Killian Neckebroeck <killian.neckebroeck@student.hogent.be>
2024-03-20 11:07:47 +0100

    registratie pagina aangemaakt

--- app/flexso/webapp/controller/Register.controller.js
+++ app/flexso/webapp/controller/Register.controller.js
@@ -1,13 +1,23 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller"],
+    ["sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
       "use strict";
   
       return Controller.extend("flexso.controller.Register", {
-        onInit: function () {},
+        onInit: function () {
+            var oRootPath = jQuery.sap.getModulePath(
+                "flexso", 
+                "/images/Flexso.png"
+                );
+            
+            var oImageModel = new JSONModel({
+                path: oRootPath
+            });
+            this.getView().setModel(oImageModel, "imageModel");
+        },
       });
     }
   );
