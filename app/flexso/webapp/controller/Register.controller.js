bc936125ba2008df8301299b5245efb05c454d2b
Killian Neckebroeck <killian.neckebroeck@student.hogent.be>
2024-03-20 12:42:41 +0100

    controller pagina registratie beter gemaakt

--- app/flexso/webapp/controller/Register.controller.js
+++ app/flexso/webapp/controller/Register.controller.js
@@ -1,9 +1,9 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel"],
+    ["sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel", "sap/m/MessageToast", "sap/ui/core/UIComponent"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, MessageToast, UIComponent) {
       "use strict";
   
       return Controller.extend("flexso.controller.Register", {
@@ -18,6 +18,40 @@ sap.ui.define(
             });
             this.getView().setModel(oImageModel, "imageModel");
         },
+        onRegister: function () {
+            var email = this.getView().byId("emailInput").getValue();
+            var company = this.getView().byId("companyInput").getValue();
+            var role = this.getView().byId("roleInput").getValue();
+            var password = this.getView().byId("passwordInput").getValue();
+            var passwordRepeat = this.getView().byId("confirmPasswordInput").getValue();
+            
+            function isValidEmail(email){
+                var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                return emailRegex.test(email);
+            }
+
+            if (!isValidEmail(email)){
+                MessageToast.show("Invalid email address!");
+            }
+
+            if (password !== passwordRepeat){
+                MessageToast.show("passwords do not match!");
+            }
+
+            if (email === "" || company === "" || role === "" || password === "" || passwordRepeat === ""){
+                MessageToast.show("Please fill in all fields!");
+            } else if (isValidEmail(email) && password === passwordRepeat){
+                MessageToast.show("Registration successful!");
+                setTimeout(
+                    function () {
+                        var oRouter = UIComponent.getRouterFor(this);
+                        oRouter.navTo("login");
+                    }.bind(this),
+                    1000
+                );
+            }
+
+        }
       });
     }
   );
