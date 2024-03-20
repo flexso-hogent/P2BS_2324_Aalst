7f3297a7a4c00aabb5e271b98919db6a13bf0934
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-03-20 09:05:36 +0100

    Kijken hoe iets werkt

--- app/flexso/webapp/controller/Login.controller.js
+++ app/flexso/webapp/controller/Login.controller.js
@@ -1,11 +1,29 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+  ],
+  function (Controller, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("flexso.controller.Login", {
+      onInit: function () {
+        var oRootPath = jQuery.sap.getModulePath(
+          "flexso",
+          "/images/Flexso.png"
+        );
+        var oImageModel = new JSONModel({
+          path: oRootPath,
+        });
+        this.getView().setModel(oImageModel, "imageModel");
+      },
+
       onLoginPress: function () {
         MessageToast.show("Login pressed");
+      },
+      onForgotPasswordPress: function () {
+        MessageToast.show("Forgot password pressed");
       },
     });
   }
