6a4f0709a962f761284ae96bb98f23016b38f4cd
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-03-21 19:13:42 +0100

    Backdata

--- app/flexso/webapp/controller/Overview.controller.js
+++ app/flexso/webapp/controller/Overview.controller.js
@@ -2,52 +2,29 @@ sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/UIComponent",
     "sap/m/MessageToast",
+    "jquery.sap.global", // Import jQuery
   ],
-  function (Controller, JSONModel, UIComponent, MessageToast) {
+  function (Controller, JSONModel, MessageToast, jQuery) {
     "use strict";
 
-    return Controller.extend("flexso.controller.overview", {
+    return Controller.extend("flexso.controller.Overview", {
       onInit: function () {
-        var eventData = [
-          {
-            eventName: "Event 1",
-            eventDate: "2024-04-01",
-            description: "Description of Event 1",
-          },
-          {
-            eventName: "Event 2",
-            eventDate: "2024-04-15",
-            description: "Description of Event 2",
-          },
-        ];
-
-        var eventModel = new JSONModel(eventData);
-        this.getView().setModel(eventModel, "eventModel");
+        this.loadData();
       },
-
-      onEventBlockPress: function (oEvent) {
-        var oItem = oEvent.getSource();
-        var oBindingContext = oItem.getBindingContext("eventModel");
-        var sEventName = oBindingContext.getProperty("eventName");
-        var sEventDate = oBindingContext.getProperty("eventDate");
-        var sDescription = oBindingContext.getProperty("description");
-
-        MessageToast.show(
-          "Event Name: " +
-            sEventName +
-            "\nEvent Date: " +
-            sEventDate +
-            "\nDescription: " +
-            sDescription
-        );
-
-        var oRouter = UIComponent.getRouterFor(this);
-        oRouter.navTo("eventDetails", {
-          eventName: sEventName,
-          eventDate: sEventDate,
-          description: sDescription,
+      loadData: function () {
+        var that = this;
+        jQuery.ajax({
+          url: "http://localhost:4004/odata/v4/catalog/Events",
+          dataType: "json",
+          success: function (data) {
+            var events = data.value;
+            var eventModel = new JSONModel(events);
+            that.getView().setModel(eventModel, "eventModel");
+          },
+          error: function (xhr, status, error) {
+            MessageToast.show("Error fetching data: " + error);
+          },
         });
       },
     });
