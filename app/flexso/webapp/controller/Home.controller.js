0bb5d84b0a550cb318b64d90215076bd66cc79eb
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-03-21 14:45:51 +0100

    Login doen verwijzen naar homepage

---
+++ app/flexso/webapp/controller/Home.controller.js
@@ -0,0 +1,43 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/UIComponent",
+  ],
+  function (Controller, JSONModel, UIComponent) {
+    "use strict";
+
+    return Controller.extend("flexso.controller.Home", {
+      onInit: function () {
+        var oProfileImagePath = jQuery.sap.getModulePath(
+          "flexso",
+          "/images/profile.jpg"
+        );
+        var oImageModel = new JSONModel({
+          profileImagePath: oProfileImagePath,
+        });
+
+        this.getView().setModel(oImageModel, "imageModel");
+      },
+
+      onSwitchToEnglish: function () {
+        var oResourceModel = this.getView().getModel("i18n");
+        oResourceModel.sLocale = "en";
+        sap.ui.getCore().getConfiguration().setLanguage("en");
+        this.getView().getModel("i18n").refresh();
+      },
+
+      onSwitchToDutch: function () {
+        var oResourceModel = this.getView().getModel("i18n");
+        oResourceModel.sLocale = "nl";
+        sap.ui.getCore().getConfiguration().setLanguage("nl");
+        this.getView().getModel("i18n").refresh();
+      },
+
+      onProfileButtonClick: function () {
+        var oRouter = UIComponent.getRouterFor(this);
+        oRouter.navTo("profile");
+      },
+    });
+  }
+);
