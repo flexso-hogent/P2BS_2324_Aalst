b61b260098624464a2ed137e2812fe96b4acd6ae
Killian Neckebroeck <killian.neckebroeck@student.hogent.be>
2024-03-28 18:33:51 +0100

    feedbackController mooier gemaakt

--- app/flexso/webapp/controller/feedback.controller.js
+++ app/flexso/webapp/controller/feedback.controller.js
@@ -20,28 +20,6 @@ sap.ui.define(
   ) {
     "use strict";
 
-    function generateUUID() {
-      var dt = new Date().getTime();
-      var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-        /[xy]/g,
-        function (c) {
-          var r = (dt + Math.random() * 16) % 16 | 0;
-          dt = Math.floor(dt / 16);
-          return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-        }
-      );
-      return uuid;
-    }
-
-    var feedbackIDCounter = parseInt(localStorage.getItem("feedbackIDCounter"));
-
-    function generateFeedbackId() {
-      var feedbackID = "fb" + feedbackIDCounter;
-      feedbackIDCounter++;
-      localStorage.setItem("feedbackIDCounter", feedbackIDCounter);
-      return feedbackID;
-    }
-
     return Controller.extend("flexso.controller.feedback", {
       onInit: function () {
         var oRootPath = jQuery.sap.getModulePath(
