ba3db0980451441b0ab26f0d6a4c07f1c2123d44
Killian Neckebroeck <killian.neckebroeck@student.hogent.be>
2024-03-29 15:06:32 +0100

    profileButton fixed on feedbackpage

--- app/flexso/webapp/controller/feedback.controller.js
+++ app/flexso/webapp/controller/feedback.controller.js
@@ -90,7 +90,7 @@ sap.ui.define(
         var oRouter = UIComponent.getRouterFor(this);
         oRouter.navTo("home");
       },
-      onProfilePressed: function () {
+      onProfileButtonClick: function () {
         var oRouter = UIComponent.getRouterFor(this);
         oRouter.navTo("profile");
       },
