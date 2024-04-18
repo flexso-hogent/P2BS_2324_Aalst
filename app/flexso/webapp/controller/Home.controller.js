96534d5c0b4a547151d2c152c2b00869e8352ed2
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-04-18 16:31:14 +0200

    Added buttons to the homepage

--- app/flexso/webapp/controller/Home.controller.js
+++ app/flexso/webapp/controller/Home.controller.js
@@ -241,6 +241,10 @@ sap.ui.define(
         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
         oRouter.navTo("allFeedback");
       },
+
+      goToFeedbackDirect: function () {},
+
+      goToSchrijfUit: function () {},
     });
   }
 );
