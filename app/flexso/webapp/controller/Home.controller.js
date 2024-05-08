d75aa3c34da8c70d6d828d82c20ff2c31d25f161
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-05-08 10:48:10 +0200

    Only 2 sessions on homepage fix

--- app/flexso/webapp/controller/Home.controller.js
+++ app/flexso/webapp/controller/Home.controller.js
@@ -84,7 +84,7 @@ sap.ui.define(
 
             // Update the model with the fetched registered session data
             var oModel = this.getView().getModel("imageModel");
-            oModel.setProperty("/registeredSessionsData", upcomingSessions);
+            oModel.setProperty("/registeredSessionsData", firstTwoSessions);
           }.bind(this),
           error: function (xhr, status, error) {
             MessageToast.show(
