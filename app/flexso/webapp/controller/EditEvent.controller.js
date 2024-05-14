cdfd52dc232b82e5818bd1ba4ccc783ed6d828be
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-05-14 13:24:40 +0200

    FixedNavigation

--- app/flexso/webapp/controller/EditEvent.controller.js
+++ app/flexso/webapp/controller/EditEvent.controller.js
@@ -1,22 +1,7 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
 
-    return Controller.extend("flexso.controller.EditEvent", {
-      onSaveEvent: function () {
-        // Implement saving logic here
-        MessageToast.show(
-          this.getView().getModel("i18n").getProperty("eventUpdated")
-        );
-      },
-
-      onDeleteEvent: function () {
-        // Implement deletion logic here
-        MessageToast.show(
-          this.getView().getModel("i18n").getProperty("eventDeleted")
-        );
-      },
-    });
-  }
-);
+  return Controller.extend("flexso.controller.EditEvent", {
+    onInit: function () {},
+  });
+});
