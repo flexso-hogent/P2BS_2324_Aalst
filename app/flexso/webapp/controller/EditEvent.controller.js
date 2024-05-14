8b51d9d9ae5b4eb3a6cb04c3aad434a1a79fa3df
KYNO VAN DE VELDE <KYNO.VANDEVELDE@student.hogent.be>
2024-05-14 14:00:50 +0200

    Navbar works

--- app/flexso/webapp/controller/EditEvent.controller.js
+++ app/flexso/webapp/controller/EditEvent.controller.js
@@ -1,7 +1,72 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent"],
+  function (Controller, UIComponent) {
+    "use strict";
 
-  return Controller.extend("flexso.controller.Editevent", {
-    onInit: function () {},
-  });
-});
+    return Controller.extend("flexso.controller.Editevent", {
+      onInit: function () {},
+
+      onBackToHome: function () {
+        var oRouter = UIComponent.getRouterFor(this);
+        oRouter.navTo("home");
+      },
+      onDropdownPress: function (oEvent) {
+        var oButton = oEvent.getSource();
+        var oPopover = this.getView().byId("popover");
+
+        if (!oPopover.isOpen()) {
+          oPopover.openBy(oButton);
+        } else {
+          oPopover.close();
+        }
+      },
+      onSwitchToEnglish: function () {
+        var oResourceModel = this.getView().getModel("i18n");
+        oResourceModel.sLocale = "en";
+        sap.ui.getCore().getConfiguration().setLanguage("en");
+        this.getView().getModel("i18n").refresh();
+      },
+
+      onSwitchToDutch: function () {
+        var oResourceModel = this.getView().getModel("i18n");
+        oResourceModel.sLocale = "nl";
+        sap.ui.getCore().getConfiguration().setLanguage("nl");
+        this.getView().getModel("i18n").refresh();
+      },
+      onProfileButtonClick: function () {
+        var oRouter = UIComponent.getRouterFor(this);
+        oRouter.navTo("profile");
+      },
+      onLogoutPress: function () {
+        var that = this;
+        sap.m.MessageBox.confirm(
+          this.getView().getModel("i18n").getProperty("logout"),
+          {
+            title: "Confirm",
+            onClose: function (oAction) {
+              if (oAction === sap.m.MessageBox.Action.OK) {
+                localStorage.clear();
+                var oRouter = UIComponent.getRouterFor(that);
+                oRouter.navTo("login");
+              }
+            },
+          }
+        );
+
+        sap.m.MessageBox.confirm(
+          this.getView().getModel("i18n").getProperty("logout"),
+          {
+            title: "Confirm",
+            onClose: function (oAction) {
+              if (oAction === sap.m.MessageBox.Action.OK) {
+                localStorage.clear();
+                var oRouter = UIComponent.getRouterFor(that);
+                oRouter.navTo("login");
+              }
+            },
+          }
+        );
+      },
+    });
+  }
+);
